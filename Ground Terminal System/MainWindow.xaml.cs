1123b53924a8a9eb0e4a6def5d9157ee5d212fb4
7g3p <stephenperrin4@live.ca>
2021-11-08 22:23:38 -0500

    Added TelemetryData class. Tested DataGrid populating programmatically (Confirmed working; commented out tests) and getting selected row(s) in DataGrid.

--- Ground Terminal System/MainWindow.xaml.cs
+++ Ground Terminal System/MainWindow.xaml.cs
@@ -20,10 +20,20 @@ namespace Ground_Terminal_System
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<TelemetryData> teleData;
+        private int temp;
+        private object thisthingy;
+
         public MainWindow()
         {
             InitializeComponent();
             timeBar.Content = DateTime.Now;
+
+            // Hide searchBar
+            searchBar.Visibility = Visibility.Hidden;
+            searchIco.Visibility = Visibility.Hidden;
+
+            teleData = new List<TelemetryData>();
         }
 
         private void searchBar_KeyDown(object sender, KeyEventArgs e)
@@ -31,6 +41,17 @@ namespace Ground_Terminal_System
             // Check if the enter key has been pressed then....???
             if (e.Key == Key.Enter)
             {
+                //teleData.Add(new TelemetryData("test1", DateTime.Now, -1.0, -1.0 - 1.0, -1.0, -1.0, -1.0, -1.0, -1.0));
+                //teleData.Add(new TelemetryData("test2", DateTime.Now, -2.0, -2.0 - 2.0, -2.0, -2.0, -2.0, -2.0, -2.0));
+                //teleData.Add(new TelemetryData("test3", DateTime.Now, -3.0, -3.0 - 3.0, -3.0, -3.0, -3.0, -3.0, -3.0));
+                //teleData.Add(new TelemetryData("test4", DateTime.Now, -4.0, -4.0 - 4.0, -4.0, -4.0, -4.0, -4.0, -4.0));
+                //teleData.Add(new TelemetryData("test5", DateTime.Now, -5.0, -5.0 - 5.0, -5.0, -5.0, -5.0, -5.0, -5.0));
+                //teleData.Add(new TelemetryData("test6", DateTime.Now, -6.0, -6.0 - 6.0, -6.0, -6.0, -6.0, -6.0, -6.0));
+                //teleData.Add(new TelemetryData("test7", DateTime.Now, -7.0, -7.0 - 7.0, -7.0, -7.0, -7.0, -7.0, -7.0));
+
+                //// Update DataGrid with new data list
+                //dbDataGrid.ItemsSource = teleData;
+
                 //TextBox temp = (TextBox)sender;
 
                 //timeBar.Content = temp.Text;
@@ -46,13 +67,40 @@ namespace Ground_Terminal_System
             if (rtCheckbox.IsChecked == true)
             {
                 realTimeData.Visibility = Visibility.Visible;
-                // dbData.Visibility = Visibility.Hidden;
+                dbData.Visibility = Visibility.Hidden;
+
+                // Hide searchBar
+                searchBar.Visibility = Visibility.Hidden;
+                searchIco.Visibility = Visibility.Hidden;
+
+                // Reveal timeBar
+                timeBar.Visibility = Visibility.Visible;
+                timeText.Visibility = Visibility.Visible;
             }
             else
             {
                 realTimeData.Visibility = Visibility.Hidden;
-                // dbData.Visibility = Visibility.Visible;
+                dbData.Visibility = Visibility.Visible;
+
+                // Reveal searchBar
+                searchBar.Visibility = Visibility.Visible;
+                searchIco.Visibility = Visibility.Visible;
+
+                // Hide timeBar
+                timeBar.Visibility = Visibility.Hidden;
+                timeText.Visibility = Visibility.Hidden;
+
+                // Update DataGrid with new data list
+                //dbDataGrid.ItemsSource = teleData;
+                tailNum.Text = temp.ToString();
             }
+        }
+
+        private void dbDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Variables
+            DataGrid dg = (DataGrid)sender;         // Cast sender as DataGrid object
+            thisthingy = dg.SelectedItems;          // Save the selected items
         }
     }
 }
---
+++ Ground Terminal System/TelemetryData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ground_Terminal_System
+{
+    public class TelemetryData
+    {
+        public string aircraftTailNumber { get; set; }
+        public DateTime timestamp { get; set; }
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+        public double weight { get; set; }
+        public double altitude { get; set; }
+        public double pitch { get; set; }
+        public double bank { get; set; }
+
+        public TelemetryData(string atNum, DateTime ts, double _x, double _y, double _z, double w, double alt, double p, double b)
+        {
+            aircraftTailNumber = atNum;
+            timestamp = ts;
+            x = _x;
+            y = _y;
+            z = _z;
+            weight = w;
+            altitude = alt;
+            pitch = p;
+            bank = b;
+        }
+    }
+}
